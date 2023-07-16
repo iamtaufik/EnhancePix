3f5993256f4f897e94ab5ed14c4069c4b11e5fd2
Shin865 <129364414+Shin865@users.noreply.github.com>
2023-07-17 04:45:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/api/upload', async (req, res) => {
     res.status(500).json({ err: 'Something went wrong' });
   }
 });
-const port = process.env.PORT || 6483;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('');
 });
